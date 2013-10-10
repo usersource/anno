639c9846f50470d12808c5527edf5c8365d6725c
David Lee <allmywant@gmail.com>
2013-10-10 20:37:27 +0800

    Add anno-gesture support to Intro and Feedback (#25)

--- anno_plugin_android/src/io/usersource/annoplugin/view/IntroActivity.java
+++ anno_plugin_android/src/io/usersource/annoplugin/view/IntroActivity.java
@@ -1,5 +1,11 @@
 package io.usersource.annoplugin.view;
 
+import android.content.Intent;
+import android.gesture.GestureOverlayView;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import io.usersource.annoplugin.AnnoPlugin;
+import io.usersource.annoplugin.utils.PluginUtils;
 import org.apache.cordova.DroidGap;
 
 import android.os.Bundle;
@@ -7,10 +13,26 @@ import android.view.KeyEvent;
 import android.view.View;
 
 public class IntroActivity extends DroidGap {
+  private int level;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     super.init();
+
+    GestureOverlayView view = new GestureOverlayView(this);
+    view.setLayoutParams(new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT, 1));
+
+    setContentView(view);
+    view.addView((View) appView.getParent());
+    view.getChildAt(0).setLayoutParams(
+            new FrameLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    FrameLayout.LayoutParams.MATCH_PARENT, 1));
+
+    setContentView(view);
+    AnnoPlugin.setEnableGesture(this, view, true);
 
     this.appView.setOnKeyListener(new View.OnKeyListener() {
 
@@ -25,6 +47,14 @@ public class IntroActivity extends DroidGap {
         return onKeyDown(keyCode, event);
       }
     });
+
     super.loadUrl("file:///android_asset/www/pages/intro/main.html");
+
+    Intent intent = getIntent();
+    level = intent.getIntExtra(PluginUtils.LEVEL, 0);
+  }
+
+  public int getLevel() {
+    return level;
   }
 }
--- anno_plugin_android/src/io/usersource/annoplugin/view/OptionFeedbackActivity.java
+++ anno_plugin_android/src/io/usersource/annoplugin/view/OptionFeedbackActivity.java
@@ -1,16 +1,46 @@
 package io.usersource.annoplugin.view;
 
+import android.content.Intent;
+import android.gesture.GestureOverlayView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import io.usersource.annoplugin.AnnoPlugin;
+import io.usersource.annoplugin.utils.PluginUtils;
 import org.apache.cordova.DroidGap;
 import android.os.Bundle;
 
 
 public class OptionFeedbackActivity extends DroidGap
 {
+  private int level;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     super.init();
 
+    GestureOverlayView view = new GestureOverlayView(this);
+    view.setLayoutParams(new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT, 1));
+
+    setContentView(view);
+    view.addView((View) appView.getParent());
+    view.getChildAt(0).setLayoutParams(
+            new FrameLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    FrameLayout.LayoutParams.MATCH_PARENT, 1));
+
+    setContentView(view);
+    AnnoPlugin.setEnableGesture(this, view, true);
+
     super.loadUrl("file:///android_asset/www/pages/feedback/main.html");
+
+    Intent intent = getIntent();
+    level = intent.getIntExtra(PluginUtils.LEVEL, 0);
+  }
+
+  public int getLevel() {
+    return level;
   }
 }
