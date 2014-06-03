78f1437f913fdf676c282874041edeb912a67605
Imran Ahmed <imran.ahmed@ignitesol.com>
2014-06-03 18:45:59 +0530

    Checking that only one public community should be created

--- anno_gec_server/api/community_api.py
+++ anno_gec_server/api/community_api.py
@@ -19,15 +19,18 @@ from api.utils import anno_js_client_id
 @endpoints.api(name='community', version='1.0', description='Community API',
                allowed_client_ids=[endpoints.API_EXPLORER_CLIENT_ID, anno_js_client_id])
 class CommunityApi(remote.Service):
-    """
-    Class which defines Community API v1.
-    """
+    def __init__(self):
+        self.publicCommunityType = 'public'
+
     @endpoints.method(CommunityMessage, message_types.VoidMessage, path='community', http_method='POST', name='community.insert')
     def community_insert(self, request):
         try:
+            # only one public community is allowed
+            if (request.type == self.publicCommunityType):
+                queryResultCount = Community.query(Community.type == self.publicCommunityType).count()
+                if queryResultCount:
+                    return message_types.VoidMessage()
             Community.insert(request)
         except BadValueError as e:
             logging.exception("Exception while inserting community: %s" % e)
         return message_types.VoidMessage()
-    
-        
