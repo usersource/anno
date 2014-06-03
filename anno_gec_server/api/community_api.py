f2cb2d1ace80945e86e01955ae12f81b568579c5
Imran Ahmed <iahmed213@gmail.com>
2014-06-03 23:33:21 +0530

    Code refactor

--- anno_gec_server/api/community_api.py
+++ anno_gec_server/api/community_api.py
@@ -1,8 +1,8 @@
-__author__ = 'rekenerd'
+__author__ = "rekenerd"
 
-"""
+'''
 Community API implemented using Google Cloud Endpoints.
-"""
+'''
 
 import logging
 
@@ -17,22 +17,27 @@ from message.common_message import StringMessage
 from model.community import Community
 from api.utils import anno_js_client_id
 
-@endpoints.api(name='community', version='1.0', description='Community API',
+@endpoints.api(name="community", version="1.0", description="Community API",
                allowed_client_ids=[endpoints.API_EXPLORER_CLIENT_ID, anno_js_client_id])
 class CommunityApi(remote.Service):
     def __init__(self):
-        self.publicCommunityType = 'public'
+        self.communityType = dict(public="public", private="private")
 
-    @endpoints.method(CommunityMessage, StringMessage, path='community', http_method='POST', name='community.insert')
+    @endpoints.method(CommunityMessage, StringMessage, path="community", http_method="POST", name="community.insert")
     def community_insert(self, request):
         try:
+            # community should be of type 'private' or 'public'
+            if not request.type in self.communityType.values():
+                respData = StringMessage(msg="Community should be of type 'private' or 'public'")
             # only one public community is allowed
-            if (request.type == self.publicCommunityType):
-                queryResultCount = Community.query(Community.type == self.publicCommunityType).count()
+            elif (request.type == self.communityType["public"]):
+                queryResultCount = Community.query(Community.type == self.communityType["public"]).count()
                 if queryResultCount:
-                    return StringMessage(msg="Community not created. Can't create more than one public community.")
-            Community.insert(request)
-        except BadValueError as e:
+                    respData = StringMessage(msg="Community not created. Can't create more than one public community.")
+            else:
+                Community.insert(request)
+                respData = StringMessage(msg="Community created.")
+        except Exception as e:
             logging.exception("Exception while inserting community: %s" % e)
-            return StringMessage(msg="%s" % e)
-        return StringMessage(msg="Community created.")
+            respData = StringMessage(msg="%s" % e)
+        return respData
