4f8a2febae22a6dad940ea28d69a61522df51f77
Imran Ahmed <imran.ahmed@ignitesol.com>
2014-07-18 15:19:11 +0530

    [GAE Server] Code refactor

--- anno_gec_server/helper/settings.py
+++ anno_gec_server/helper/settings.py
@@ -1,8 +1,7 @@
 import logging
-
-# Figure out where we are running
+from os import environ
 from google.appengine.api import app_identity
-_default_hostname = app_identity.get_default_version_hostname()
+
 
 # Client ID for web application
 JS_CLIENT_ID_PROD_USERSOURCE = "22913132792.apps.googleusercontent.com"
@@ -19,45 +18,58 @@ GCM_API_KEY_PROD_USERSOURCE = "AIzaSyD11tLsJXp9HNHWd33ZGvzCwbxjeMlkryk"
 GCM_API_KEY_TEST_USERSOURCE = "AIzaSyApUUBZe5Gborkwd-UknUvHdm9oblPSn9k"
 GCM_API_KEY_SANDBOX_USERSOURCE = "AIzaSyCNWf_rZCovDez9Dmzx7CA-m6IHHUmh-SU"
 
+# APNS push certificates
+APNS_PUSH_CERT_PROD_USERSOURCE = "APNS_Certificates/<WE-NEED-A-FILE>.pem"
+APNS_PUSH_CERT_TEST_USERSOURCE = "APNS_Certificates/PushChatCert.pem"
+APNS_PUSH_CERT_SANDBOX_USERSOURCE = "APNS_Certificates/PushChatCert.pem"
+
+# APNS push key
+APNS_PUSH_KEY_PROD_USERSOURCE = "APNS_Certificates/<WE-NEED-A-FILE>.pem"
+APNS_PUSH_KEY_TEST_USERSOURCE = "APNS_Certificates/PushChatKey.pem"
+APNS_PUSH_KEY_SANDBOX_USERSOURCE = "APNS_Certificates/PushChatKey.pem"
+
 # Default values
 anno_js_client_id = JS_CLIENT_ID_SANDBOX_USERSOURCE
 GCM_API_KEY = GCM_API_KEY_SANDBOX_USERSOURCE
-APNS_PUSH_CERT = 'APNS_Certificates/<WE-NEED-A-FILE>.pem'
-APNS_PUSH_KEY = 'APNS_Certificates/<WE-NEED-A-FILE>.pem'
+APNS_PUSH_CERT = APNS_PUSH_CERT_SANDBOX_USERSOURCE
+APNS_PUSH_KEY = APNS_PUSH_KEY_SANDBOX_USERSOURCE
 APNS_USE_SANDBOX = False
 
 # Enhanced Mode does not quite work on GAE Dev
 # https://groups.google.com/forum/#!topic/google-appengine/P-1Gpwpry7w
 APNS_ENHANCED = False
 
+
+# Figure out where we are running
+_default_hostname = app_identity.get_default_version_hostname()
+
 # Prod server
 if _default_hostname == DEFAULT_HOST_PROD_USERSOURCE:
     anno_js_client_id = JS_CLIENT_ID_PROD_USERSOURCE
     GCM_API_KEY = GCM_API_KEY_PROD_USERSOURCE
-    APNS_PUSH_CERT = 'APNS_Certificates/<WE-NEED-A-FILE>.pem'
-    APNS_PUSH_KEY = 'APNS_Certificates/<WE-NEED-A-FILE>.pem'
+    APNS_PUSH_CERT = APNS_PUSH_CERT_PROD_USERSOURCE
+    APNS_PUSH_KEY = APNS_PUSH_KEY_PROD_USERSOURCE
     APNS_USE_SANDBOX = False
 
 # Test server
 elif _default_hostname == DEFAULT_HOST_TEST_USERSOURCE:
     anno_js_client_id = JS_CLIENT_ID_TEST_USERSOURCE
     GCM_API_KEY = GCM_API_KEY_TEST_USERSOURCE
-    APNS_PUSH_CERT = 'APNS_Certificates/<WE-NEED-A-FILE>.pem'
-    APNS_PUSH_KEY = 'APNS_Certificates/<WE-NEED-A-FILE>.pem'
+    APNS_PUSH_CERT = APNS_PUSH_CERT_TEST_USERSOURCE
+    APNS_PUSH_KEY = APNS_PUSH_KEY_TEST_USERSOURCE
     APNS_USE_SANDBOX = False
 
 # Dev server
 elif _default_hostname == DEFAULT_HOST_SANDBOX_USERSOURCE:
     anno_js_client_id = JS_CLIENT_ID_SANDBOX_USERSOURCE
     GCM_API_KEY = GCM_API_KEY_SANDBOX_USERSOURCE
-    APNS_PUSH_CERT = 'APNS_Certificates/<WE-NEED-A-FILE>.pem'
-    APNS_PUSH_KEY = 'APNS_Certificates/<WE-NEED-A-FILE>.pem'
+    APNS_PUSH_CERT = APNS_PUSH_CERT_SANDBOX_USERSOURCE
+    APNS_PUSH_KEY = APNS_PUSH_KEY_SANDBOX_USERSOURCE
     APNS_USE_SANDBOX = False
 
 # Are we in the local development environment
-from os import environ
 DEVELOPMENT = False
-if "Development" in environ.get('SERVER_SOFTWARE', ''):
+if "Development" in environ.get("SERVER_SOFTWARE", ""):
     DEVELOPMENT = True
 
 logging.getLogger().info("Running on %s, under development: %s", _default_hostname, DEVELOPMENT)
@@ -70,17 +82,17 @@ if DEVELOPMENT:
 
     # Add to the Whitelist
     from google.appengine.tools.devappserver2.python import sandbox
-    sandbox._WHITE_LIST_C_MODULES += ['_ssl', '_socket']
+    sandbox._WHITE_LIST_C_MODULES += ["_ssl", "_socket"]
 
     # Use the stdlib Python socket rather than Googles wrapped stub
     from lib import socket as patched_socket
-    sys.modules['socket'] = patched_socket
+    sys.modules["socket"] = patched_socket
     socket = patched_socket
 
     # Dev Keys and certificates
     GCM_API_KEY = GCM_API_KEY_SANDBOX_USERSOURCE  # Already existing Public Key for Google Services
-    APNS_PUSH_CERT = 'APNS_Certificates/aps_development.pem'
-    APNS_PUSH_KEY = 'APNS_Certificates/PushNotificationsDev.pem'
+    APNS_PUSH_CERT = "APNS_Certificates/aps_development.pem"
+    APNS_PUSH_KEY = "APNS_Certificates/PushNotificationsDev.pem"
 
     # Use the APNS sandbox environment
     APNS_USE_SANDBOX = True
