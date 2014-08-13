0b2c310e5ba72acac6fce8ca3b79c3119a296e84
Imran Ahmed <imran.ahmed@ignitesol.com>
2014-08-13 11:52:16 +0530

    [GAE Server] Minor fixes

--- anno_gec_server/api/anno_api.py
+++ anno_gec_server/api/anno_api.py
@@ -289,8 +289,12 @@ class AnnoApi(remote.Service):
         user = auth_user(self.request_state.headers)
         userannostate_list = UserAnnoState.list_by_user(user.key)
         anno_key_list = [ userannostate.anno for userannostate in userannostate_list ]
-        anno_list = Anno.query(Anno.key.IN(anno_key_list)).order(-Anno.last_update_time).fetch()
-        anno_message_list = [ anno.to_response_message(user) for anno in anno_list if anno is not None ]
+
+        anno_message_list = []
+        if len(anno_key_list):
+            anno_list = Anno.query(Anno.key.IN(anno_key_list)).order(-Anno.last_update_time).fetch()
+            anno_message_list = [ anno.to_response_message(user) for anno in anno_list if anno is not None ]
+
         return AnnoListMessage(anno_list=anno_message_list)
 
 
