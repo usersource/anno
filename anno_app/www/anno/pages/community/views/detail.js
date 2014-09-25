0116045d2f1f365724fe4c838523f93285f49d0a
Imran Ahmed <imran.ahmed@ignitesol.com>
2014-09-25 15:57:36 +0530

    [Client] Calling 'resetDetailpage' when coming back from detail page

--- anno_app/www/anno/pages/community/views/detail.js
+++ anno_app/www/anno/pages/community/views/detail.js
@@ -431,30 +431,25 @@ define([
             return false;
         };
 
-        var handleBackButton = function()
-        {
-            var zoomData = dom.byId('zoomScreenshotContainerDetail');
+        var handleBackButton = function() {
+            var zoomData = dom.byId('zoomScreenshotContainerDetail'),
+                dlg = registry.byId('dlg_common_confirm_message');
+
             if (zoomData && (zoomData.style.display == '' || zoomData.style.display == 'block')) {
                 // Analytics
                 annoUtil.actionGATracking(annoUtil.analytics.category.detail, 'zoom close', 'android back');
-
                 zoomClose();
                 return;
             }
 
-            var dlg = registry.byId('dlg_common_confirm_message');
-
-            if (dlg&&(dlg.domNode.style.display == ''||dlg.domNode.style.display == 'block'))
-            {
+            if (dlg && (dlg.domNode.style.display == '' || dlg.domNode.style.display == 'block')) {
                 dlg.hide();
-            }
-            else
-            {
+            } else {
                 // Analytics
                 annoUtil.actionGATracking(annoUtil.analytics.category.detail, 'android back button');
-
                 app.setBackwardFired(true);
                 history.back();
+                resetDetailPage();
             }
         };
 
