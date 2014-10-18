14fa241872d4f498d50d555bc622d4f897836094
Imran Ahmed <imran.ahmed@ignitesol.com>
2014-10-18 10:41:20 +0530

    [Server] Checking for circle level before creating an anno

--- anno_gec_server/model/anno.py
+++ anno_gec_server/model/anno.py
@@ -135,7 +135,8 @@ class Anno(BaseModel):
 
         circle_level = 0
         if community:
-            circle_level = message.circle_level or UserRole.getCircleLevel(user, community)
+            userrole_circle_level = UserRole.getCircleLevel(user, community)
+            circle_level = message.circle_level if (message.circle_level <= userrole_circle_level) else userrole_circle_level
 
         entity = cls(anno_text=message.anno_text, anno_type=message.anno_type,
                      level=message.level, device_model=message.device_model, 
