5b31f25421f5ebf53ce8ee161f306ffb07ff4ea6
Imran Ahmed <imran.ahmed@ignitesol.com>
2014-12-22 14:51:32 +0530

    [DemoApp][Android] Getting user info

--- UserSourcePluginDemo/android/src/io/usersource/demoapp/LoginFragment.java
+++ UserSourcePluginDemo/android/src/io/usersource/demoapp/LoginFragment.java
@@ -2,9 +2,14 @@ package io.usersource.demoapp;
 
 import java.util.Arrays;
 
+
+import com.facebook.Request;
+import com.facebook.RequestBatch;
+import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
 
 import android.content.Intent;
@@ -19,6 +24,8 @@ public class LoginFragment extends Fragment {
 	private static final String TAG = "LoginFragment";
 	private UiLifecycleHelper uiHelper;
 
+	String name, email, image_url;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
@@ -31,7 +38,7 @@ public class LoginFragment extends Fragment {
 		View view = inflater.inflate(R.layout.main, container, false);
 
 		LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
-		authButton.setReadPermissions(Arrays.asList("public_profile"));
+		authButton.setReadPermissions(Arrays.asList("public_profile", "email"));
 		authButton.setFragment(this);
 
 		return view;
@@ -49,7 +56,36 @@ public class LoginFragment extends Fragment {
 	    @Override
 	    public void call(Session session, SessionState state, Exception exception) {
 	        onSessionStateChange(session, state, exception);
+	        getUserInfo(session, state);
 	    }
+
+		private void getUserInfo(Session session, SessionState state) {
+			if (state.isOpened()) {
+				RequestBatch requestBatch = new RequestBatch();
+				Request detailRequest = getUserProfileDetails(session);
+				requestBatch.add(detailRequest);
+				requestBatch.executeAsync();
+			}
+		}
+
+		private Request getUserProfileDetails(Session session) {
+			return Request.newMeRequest(session, new Request.GraphUserCallback() {
+	            @Override
+	            public void onCompleted(GraphUser user, Response response) {
+	                if (response != null) {
+	                    try {
+	                        name = user.getName();
+	                        email = (String) user.getProperty("email");
+							image_url = "http://graph.facebook.com/" + user.getId() + "/picture";
+	                        Log.d(TAG, "Name: " + name + " Email: " + email + "Image URL: " + image_url);
+	                    } catch (Exception e) {
+	                        e.printStackTrace();
+	                        Log.e(TAG, "Exception e");
+	                    }
+	                }
+	            }
+	        });
+		}
 	};
 
 	@Override
