63d5c7db978138898027d674683f395c55d51899
Imran Ahmed <imran.ahmed@ignitesol.com>
2014-12-22 19:22:03 +0530

    [DemoApp][Android] Showing albums

--- UserSourcePluginDemo/android/src/io/usersource/demoapp/LoginActivity.java
+++ UserSourcePluginDemo/android/src/io/usersource/demoapp/LoginActivity.java
@@ -1,17 +1,45 @@
 package io.usersource.demoapp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class LoginActivity extends FragmentActivity {
 	private LoginFragment loginFragment;
 	static Menu mainMenu;
 
+	private GridView gridView;
+	private GridViewAdapter customGridAdapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+		gridView = (GridView) findViewById(R.id.gridView);
+		customGridAdapter = new GridViewAdapter(this, R.layout.row_grid, getData());
+		gridView.setAdapter(customGridAdapter);
+
+		gridView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+				Toast.makeText(LoginActivity.this, position + "#Selected", Toast.LENGTH_SHORT).show();
+			}
+		});
 
 	    if (savedInstanceState == null) {
 	        loginFragment = new LoginFragment();
@@ -19,6 +47,26 @@ public class LoginActivity extends FragmentActivity {
 	    } else {
 	        loginFragment = (LoginFragment) getSupportFragmentManager().findFragmentById(android.R.id.content);
 	    }
+	}
+
+	private ArrayList<ImageItem> getData() {
+		final ArrayList<ImageItem> imageItems = new ArrayList<ImageItem>();
+
+		File picturePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+		for (File file : picturePath.listFiles()) {
+			File imageUri = file.listFiles()[0];
+	        Bitmap bitmap = null;
+			try {
+				bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.fromFile(imageUri));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			imageItems.add(new ImageItem(bitmap, file.getName()));
+		}
+
+		return imageItems;
 	}
 
 	@Override
