52f8873323c88a4559410dff686dbe7812a3dc6d
Imran Ahmed <imran.ahmed@ignitesol.com>
2014-12-23 18:28:41 +0530

    [DemoApp][Android] Showing dialog to select shake sensitivity value

--- UserSourcePluginDemo/android/src/io/usersource/demoapp/SettingsActivity.java
+++ UserSourcePluginDemo/android/src/io/usersource/demoapp/SettingsActivity.java
@@ -3,13 +3,20 @@ package io.usersource.demoapp;
 import io.usersource.annoplugin.AnnoSingleton;
 import io.usersource.annoplugin.shake.ShakeEnabler;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckedTextView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class SettingsActivity extends Activity {
 	private AnnoSingleton annoSingleton = null;
+
+	CheckedTextView checkedTextView;
+	TextView shakeValueTextView;
+	RelativeLayout shakeSensitivityLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,22 +24,37 @@ public class SettingsActivity extends Activity {
 		setContentView(R.layout.settings);
 
 		annoSingleton = AnnoSingleton.getInstance(this);
-	    ShakeEnabler.startListening(this);
+		ShakeEnabler.startListening(this);
 
-		final CheckedTextView checkedTextView = (CheckedTextView) findViewById(R.id.allowShake);
+		shakeSensitivityLayout = (RelativeLayout) findViewById(R.id.shakeSensitivity);
+		shakeSensitivityLayout.setClickable(AnnoSingleton.allowShake);
+
+		checkedTextView = (CheckedTextView) findViewById(R.id.allowShake);
 		checkedTextView.setChecked(AnnoSingleton.allowShake);
 		checkedTextView.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View v) {
-				if (checkedTextView.isChecked())
-					checkedTextView.setChecked(false);
-				else
-					checkedTextView.setChecked(true);
-				annoSingleton.saveAllowShake(checkedTextView.isChecked());
+			public void onClick(View view) {
+				Boolean allowShakeValue = checkedTextView.isChecked();
+				checkedTextView.setChecked(allowShakeValue);
+				shakeSensitivityLayout.setClickable(allowShakeValue);
+				annoSingleton.saveAllowShake(allowShakeValue);
 			}
 		});
 
-		TextView shakeSensitivityValue = (TextView) findViewById(R.id.shakeSensitivityValue);
-		shakeSensitivityValue.setText(AnnoSingleton.shakeSensitivityValues.get(AnnoSingleton.shakeValue));
+		shakeValueTextView = (TextView) findViewById(R.id.shakeSensitivityValue);
+		shakeValueTextView.setText(AnnoSingleton.shakeSensitivityValues.get(AnnoSingleton.shakeValue));
+	}
+
+	public void showShakeSensitivityDialog(View view) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Choose Shake Sensitivity Value").setItems(
+				R.array.shakeSensitivityValues,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						annoSingleton.saveShakeValue(which);
+						shakeValueTextView.setText(AnnoSingleton.shakeSensitivityValues.get(which));
+					}
+				});
+		builder.create().show();
 	}
 }
