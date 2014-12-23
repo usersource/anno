209d81ed2388041759978fba5b41bfa0a1504794
Imran Ahmed <imran.ahmed@ignitesol.com>
2014-12-23 12:16:25 +0530

    [DemoApp][Android] Updated UI

--- UserSourcePluginDemo/android/src/io/usersource/demoapp/adapter/FullScreenImageAdapter.java
+++ UserSourcePluginDemo/android/src/io/usersource/demoapp/adapter/FullScreenImageAdapter.java
@@ -13,7 +13,6 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -41,25 +40,16 @@ public class FullScreenImageAdapter extends PagerAdapter {
 	@Override
 	public Object instantiateItem(ViewGroup container, int position) {
 		ImageView imgDisplay;
-		Button btnClose;
 
 		inflater = (LayoutInflater) _activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View viewLayout = inflater.inflate(R.layout.layout_fullscreen_image, container, false);
 
 		imgDisplay = (ImageView) viewLayout.findViewById(R.id.imgDisplay);
-		btnClose = (Button) viewLayout.findViewById(R.id.btnClose);
 
 		BitmapFactory.Options options = new BitmapFactory.Options();
 		options.inPreferredConfig = Bitmap.Config.ARGB_8888;
 		Bitmap bitmap = BitmapFactory.decodeFile(_imagePaths.get(position), options);
 		imgDisplay.setImageBitmap(bitmap);
-
-		btnClose.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				_activity.finish();
-			}
-		});
 
 		((ViewPager) container).addView(viewLayout);
 		return viewLayout;
