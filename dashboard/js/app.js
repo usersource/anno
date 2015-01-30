28b3103471bb29ae7ebf3cffe05561ffaeb526df
Imran Ahmed <iahmed213@gmail.com>
2015-01-30 23:43:12 +0530

    [Dashboard] Show 'No Notes' for team notes when no team note present

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -52,8 +52,13 @@ Dashboard.controller('Feed', function($scope, $cookieStore, DataService, Dashboa
         angular.forEach($scope.annoList, function(anno) {
 			anno.tags = [];
 			anno.mentions = [];
+			anno.teamNotesNotPresent = false;
 			if (anno.device_model in DashboardConstants.deviceList) {
 				anno.device_model = DashboardConstants.deviceList[anno.device_model];
+			}
+			if (anno.team_notes.length === 0) {
+				anno.teamNotesNotPresent = true;
+				anno.team_notes = "No Notes";
 			}
         });
     });
