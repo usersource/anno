33fc4bcfa1184e1fda03ef6e144475f1aba232b1
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-02-17 18:00:12 +0530

    [Dashboard] Refactor code

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -10,24 +10,13 @@ Dashboard.config(function($routeProvider, $locationProvider) {
         $cookies = _$cookies_;
     });
 
-    $routeProvider.when('/dashboard/:teamHash/:teamName/login', {
+    $routeProvider.when('/dashboard/:teamHash?/:teamName?/login', {
         templateUrl: '/dashboard/partials/login.html',
         controller: 'Login'
-    }).when('/dashboard/:teamHash/:teamName/feed', {
+    }).when('/dashboard/:teamHash?/:teamName?/feed', {
         templateUrl: '/dashboard/partials/feed.html',
         controller: 'Feed'
-    }).when('/dashboard/:teamHash/:teamName', {
-        redirectTo: function(params, current_url) {
-            var action_page = angular.equals($cookies.authenticated, "true") ? '/feed' : '/login';
-            return current_url + action_page;
-        }
-    }).when('/dashboard/login', {
-        templateUrl: '/dashboard/partials/login.html',
-        controller: 'Login'
-    }).when('/dashboard/feed', {
-        templateUrl: '/dashboard/partials/feed.html',
-        controller: 'Feed'
-    }).when('/dashboard', {
+    }).when('/dashboard/:teamHash?/:teamName?', {
         redirectTo: function(params, current_url) {
             var action_page = angular.equals($cookies.authenticated, "true") ? '/feed' : '/login';
             return current_url + action_page;
