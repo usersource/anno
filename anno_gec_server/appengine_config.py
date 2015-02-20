21b86fb9afc2ecde0a876926dbdce40833861989
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-02-20 19:13:15 +0530

    [Dashboard] Update appengine config

--- anno_gec_server/appengine_config.py
+++ anno_gec_server/appengine_config.py
@@ -3,5 +3,5 @@
 # See google/appengine/ext/remote_api/handler.py for more information.
 # In most cases, you will not want to configure this.
 
-trusted_appid = ["annoserver"]
+trusted_appid = ["annoserver", "annoserver-test", "usersource-anno"]
 remoteapi_CUSTOM_ENVIRONMENT_AUTHENTICATION = ("HTTP_X_APPENGINE_INBOUND_APPID", trusted_appid)
