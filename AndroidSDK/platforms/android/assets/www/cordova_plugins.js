85cf5be3f083c887aadeb34e60f054173eae0b9b
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-03-13 18:30:11 +0530

    [AndroidSDK] Update 'camera' plugin

--- AndroidSDK/platforms/android/assets/www/cordova_plugins.js
+++ AndroidSDK/platforms/android/assets/www/cordova_plugins.js
@@ -154,34 +154,6 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/org.apache.cordova.camera/www/CameraConstants.js",
-        "id": "org.apache.cordova.camera.Camera",
-        "clobbers": [
-            "Camera"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverOptions.js",
-        "id": "org.apache.cordova.camera.CameraPopoverOptions",
-        "clobbers": [
-            "CameraPopoverOptions"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.camera/www/Camera.js",
-        "id": "org.apache.cordova.camera.camera",
-        "clobbers": [
-            "navigator.camera"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverHandle.js",
-        "id": "org.apache.cordova.camera.CameraPopoverHandle",
-        "clobbers": [
-            "CameraPopoverHandle"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.network-information/www/network.js",
         "id": "org.apache.cordova.network-information.network",
         "clobbers": [
@@ -209,6 +181,34 @@ module.exports = [
         "clobbers": [
             "SQLitePlugin"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraConstants.js",
+        "id": "org.apache.cordova.camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverOptions.js",
+        "id": "org.apache.cordova.camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/Camera.js",
+        "id": "org.apache.cordova.camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverHandle.js",
+        "id": "org.apache.cordova.camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -216,10 +216,10 @@ module.exports.metadata =
 {
     "org.apache.cordova.device": "0.2.12",
     "org.apache.cordova.file": "1.3.1",
-    "org.apache.cordova.camera": "0.3.2",
     "org.apache.cordova.network-information": "0.2.12",
     "org.apache.cordova.inappbrowser": "0.5.2",
-    "com.brodysoft.sqlitePlugin": "1.0.5"
+    "com.brodysoft.sqlitePlugin": "1.0.5",
+    "org.apache.cordova.camera": "0.3.5"
 }
 // BOTTOM OF METADATA
 });
