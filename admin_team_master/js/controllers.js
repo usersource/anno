2df150895eef6be3ac878bdf614a20f3947b2dc4
Imran Ahmed <imran.ahmed@ignitesol.com>
2015-03-17 15:25:48 +0530

    Update admin team master

--- admin_team_master/js/controllers.js
+++ admin_team_master/js/controllers.js
@@ -3,6 +3,9 @@
 var AdminTeamMaster = angular.module('AdminTeamMaster', ['AdminTeamMasterConstantsModule', 'ServiceModule']);
 
 AdminTeamMaster.controller('Main', function($scope, $timeout, $location, DataService, AdminTeamMasterConstants) {
+    $scope.communities = [];
+    $scope.community_detail = {};
+
     function showAdminTeamMasterMessage(message, error_type) {
         $scope.error_message = message;
         $scope.admin_master_team_error_type = error_type || false;
@@ -21,17 +24,23 @@ AdminTeamMaster.controller('Main', function($scope, $timeout, $location, DataSer
             if ($location.port() !== "80") {
                 url = url + ":" + $location.port();
             }
-            url = url + "/dashboard/" + team_hash + "/" + community_name.replace(/\W+/g, "-");
+            url = url + "/dashboard/" + team_hash + "/" + community_name.replace(/\W+/g, "-").toLowerCase();
             return url;
         } else {
             return "";
         }
     };
 
-    $scope.getAdminTeamMasterList = function(event) {
-        $scope.communities = [];
-        $scope.community_detail = {};
+    $scope.selectCommunity = function(event) {
+        var team_key = event.currentTarget.dataset.teamKey;
+        angular.element(document.getElementsByClassName("community")).removeClass("selected");
+        angular.element(event.currentTarget).addClass("selected");
+        $scope.community_detail = $scope.communities.filter(function(community) {
+            return community.team_key === team_key;
+        })[0];
+    };
 
+    $scope.getAdminTeamMasterList = function(event) {
         DataService.makeHTTPCall("community.community.admin_master", {
         }, function(data) {
             if (data.hasOwnProperty('communities') && data.communities.length > 0) {
